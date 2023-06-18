51c2e46b6e4e473193edd400e6765c197f397ac9
Stan-G7 <stanleystan124@gmail.com>
2023-06-18 12:18:29 +0300

    Assignment Review String Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+// Define the currentUser variable
+const currentUser = 'Stan G';
+
+// Define the welcomeMessage variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the shortGreeting variable
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
